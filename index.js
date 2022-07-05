aedcdae625e7a4051efd7c02b212bb9be4331afa
mejlap <miloshp8@gmail.com>
2022-07-05 14:17:28 +0200

    final commit

--- index.js
+++ index.js
@@ -7,17 +7,16 @@ const resultKilogramsPoundsEl = document.getElementById('kilograms-pounds')
 const renderResults = () => {
     // save value from input
     const basicValue = inputEl.value
-    // calculation meters/feet
-    const meterFeet = (basicValue * 3.281).toFixed(3)
-    const feetMeter = (basicValue / 3.281).toFixed(3)
-    // calculation liters/gallons
-    const literGallon = (basicValue * 0.264).toFixed(3)
-    const gallonLiter = (basicValue / 0.264).toFixed(3)
-    // calculation kilograms/pounds
-    const kiloPound = (basicValue * 2.204).toFixed(3)
-    const poundKilo = (basicValue / 2.204).toFixed(3)
-
     if (basicValue > 0) {
+        // calculation meters/feet
+        const meterFeet = (basicValue * 3.281).toFixed(3)
+        const feetMeter = (basicValue / 3.281).toFixed(3)
+        // calculation liters/gallons
+        const literGallon = (basicValue * 0.264).toFixed(3)
+        const gallonLiter = (basicValue / 0.264).toFixed(3)
+        // calculation kilograms/pounds
+        const kiloPound = (basicValue * 2.204).toFixed(3)
+        const poundKilo = (basicValue / 2.204).toFixed(3)
         resultMeterFeetEl.textContent = `${basicValue} meters = ${meterFeet} feet | ${basicValue} feet = ${feetMeter} meters`
         resultLitersGallonsEl.textContent = `${basicValue} liters = ${literGallon} gallons | ${basicValue} gallons = ${gallonLiter} liters`
         resultKilogramsPoundsEl.textContent = `${basicValue} kilos = ${kiloPound} pounds | ${basicValue} pounds = ${poundKilo} kilos`
